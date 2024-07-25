52a6703c599bb81ec7bb83ff14472454c2983863
Daniyar410 <ramziya.shakirova@gmail.com>
2024-07-25 16:23:10 +0500

    Add files via upload

--- inedx.js
+++ inedx.js
@@ -1,16 +1,30 @@
 
 
-let number = prompt("Введите пароль:");
+// let number = prompt("Введите пароль:");
 
-number = parseFloat(number);
+// number = parseFloat(number);
 
-if (isNaN(number)) {
-    console.log("Пожалуйста, введите действительный пароль!.");
-} else if (number > 0) {
-    console.log("Молодец!");
-} else if (number < 0) {
-    console.log("Пароль неверный");
+// if (isNaN(number)) {
+//     console.log("Пожалуйста, введите действительный пароль!.");
+// } else if (number > 0) {
+//     console.log("Молодец!");
+// } else if (number < 0) {
+//     console.log("Пароль неверный");
+// } else {
+//     console.log("Доступ закрыт");
+//     alert('ЧЁ САМЫЙ УМНЫЙ')
+// }
+
+let birthYear = prompt("Введите ваш год рождения:");
+
+birthYear = parseInt(birthYear);
+
+if (isNaN(birthYear)) {
+    console.log("Пожалуйста, введите действительный год.");
 } else {
-    console.log("Доступ закрыт");
-    alert('ЧЁ САМЫЙ УМНЫЙ')
+    let currentYear = new Date().getFullYear();
+    
+    let age = currentYear - birthYear;
+    
+    console.log("Ваш возраст: " + age + " лет.");
 }
