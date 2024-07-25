62922728026c5f751567f33db830824580fbd350
Daniyar410 <ramziya.shakirova@gmail.com>
2024-07-25 16:26:44 +0500

    Add files via upload

--- inedx.js
+++ inedx.js
@@ -1,19 +1,5 @@
 
 
-// let number = prompt("Введите пароль:");
-
-// number = parseFloat(number);
-
-// if (isNaN(number)) {
-//     console.log("Пожалуйста, введите действительный пароль!.");
-// } else if (number > 0) {
-//     console.log("Молодец!");
-// } else if (number < 0) {
-//     console.log("Пароль неверный");
-// } else {
-//     console.log("Доступ закрыт");
-//     alert('ЧЁ САМЫЙ УМНЫЙ')
-// }
 
 let birthYear = prompt("Введите ваш год рождения:");
 
